98722ea9b238634923ed6095c29943a153e57dc2
Akanksha Nunakani <akanksha.nunakani5@gmail.com>
2025-06-02 17:21:02 +1000

    Refactor: Move server start to server.js for clean Jest test

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
 const apiRoutes = require('./routes/api');
 
 app.use(express.json());
 app.use('/api', apiRoutes);
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
-
-module.exports = app; // for testing
+module.exports = app; // Do NOT start the server here
